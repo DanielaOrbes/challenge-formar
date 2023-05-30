bd908c1a9a565f00a054aa7f164a41773074ab6d
Daniela Orbes <daniiorbes15@gmail.com>
2023-05-30 01:22:46 -0300

    challenge primera parte L.Node

---
+++ app.js
@@ -0,0 +1,16 @@
+const FizzBuzz = require('./fizz_buzz');
+const cardinality = require ('./number_cardinality');
+const vision = require('./range_vision');
+
+
+console.log(FizzBuzz(2));
+console.log(FizzBuzz(5));
+console.log(FizzBuzz(9));
+console.log(FizzBuzz(15));
+
+
+
+console.log(cardinality(100))
+console.log(cardinality(155))
+console.log(cardinality(88))  
+console.log(cardinality(99))
---
+++ number_cardinality.js
@@ -0,0 +1,23 @@
+/*
+Escriba la función number_cardinality() que toma un número entero y devuelve una cadena, de la siguiente manera:
+Devuelve la cadena zero si el número termina en 0
+Devuelve la cadena five si el número termina en 5
+Devuelve la cadena even si el número es par (divisible por 2) Y no termina en 0
+Devuelve la cadena odd si el número es impar (no divisible por 2) Y no termina en 5
+Asegúrese de que su cadena de retorno sea exactamente como la anterior, usando letras minúsculas.
+ */
+
+
+function number_cardinality(N) {
+    if (N % 10 === 0) {
+      return "zero"
+    } else if (N % 10 === 5) {
+      return "five"
+    } else if (N % 2 === 0) {
+      return "even"
+    } else {
+      return "odd"
+    }
+  }
+
+  module.exports = number_cardinality();
